2202ec7dc95f71a3698bebd9ff8d298ace567bff
山本健剛 <yamamotokengo@yamamotokengounoMacBook-Air.local>
2019-07-09 17:13:26 +0900

    [Finish]3-5

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,22 @@
 class PostsController < ApplicationController
   def new
+	# 空のモデルをビューに渡す
+	@post = Post.new
+  end
+
+		# 以下を追加
+
+	def create
+		# ストロングパラメーターを使用
+		post = Post.new(post_params)
+		# DBへ保存する
+		post.save
+		# トップ画面へリダイレクト
+		redirect_to '/top'
+	end
+
+  private
+  def post_params
+  		params.require(:post).permit(:title, :body)
   end
 end
