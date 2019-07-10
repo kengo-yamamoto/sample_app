7290abb49a3409a2034ce913c45bff6e5806d523
山本健剛 <yamamotokengo@yamamotokengounoMacBook-Air.local>
2019-07-10 18:42:29 +0900

    [Finish]3-7

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,7 +25,11 @@ class PostsController < ApplicationController
 	def edit
 		@post = Post.find(params[:id])
 	end
-
+	def update
+		post = Post.find(params[:id])
+		post.update(post_params)
+		redirect_to post_path(post.id)
+	end
 
   private
   def post_params
