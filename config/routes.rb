e559f8dabc0890bd40d2ffff549361bb1aa5cd95
山本健剛 <yamamotokengo@yamamotokengounoMacBook-Air.local>
2019-07-10 21:04:53 +0900

    [Start]3-9

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   #.../posts/1 や .../posts/3に該当する
   get 'posts/:id/edit' => 'posts#edit', as: 'edit_post'
   patch 'posts/:id' => 'posts#update', as: 'update_post'
+  delete 'posts/:id' => 'posts#destroy', as: 'destroy_post'
+
 end
 
