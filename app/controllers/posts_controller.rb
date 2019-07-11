8bc1ead4becdc0253071c78156a9f6d83771125e
山本健剛 <yamamotokengo@yamamotokengounoMacBook-Air.local>
2019-07-11 12:27:47 +0900

    [Finish]3-9

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,15 @@
 class PostsController < ApplicationController
-  def new
-	# 空のモデルをビューに渡す
-	@post = Post.new
-  end
+
+
+  	def new
+		@post = Post.new
+ 	end
 
 
 	def create
-		# ストロングパラメーターを使用
 		post = Post.new(post_params)
-		# DBへ保存する
 		post.save
-		# トップ画面へリダイレクト
-		redirect_to post_path(post.id) #詳細画面へリダイレクト
+		redirect_to post_path(post.id)
 	end
 
 
@@ -35,7 +33,11 @@ class PostsController < ApplicationController
 		post.update(post_params)
 		redirect_to post_path(post.id)
 	end
-
+	def destroy
+		post = Post.find(params[:id])
+		post.destroy
+		redirect_to posts_path
+	end
   private
   def post_params
   		params.require(:post).permit(:title, :body, :image)
